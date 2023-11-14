8665d5b5dd90a0849233dd193abadaffa4b6b3c9
Doruk <dorukyogscast@gmail.com>
2023-11-14 21:02:18 +0300

    added write

--- quark.go
+++ quark.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/binary"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 /*
@@ -15,27 +19,24 @@ File Structure:
     Record Count - uint8
     Records:
         filename - [40]byte
-        order - uint8
-        size - uint64
+        size 	 - int64
     Files:
-        file - any size
+        file 	 - any size
 */
 
-// Record represents the structure of each record in the file.
+// Record represents the structure of each record
 type Record struct {
-	FileName string
-	Order    uint8
-	Size     uint64
+	FileName [40]byte // [40]byte
+	Size     int64
 }
 
-// FileStructure represents the overall structure of the file.
-type FileStructure struct {
+// DatabaseStructure represents the overall structure of the database.
+type DatabaseStructure struct {
 	RecordCount uint8
 	Records     []Record
-	Files       [][]byte
 }
 
-var cursor_position uint64 = 0
+var cursor_position int64 = 0
 
 func main() {
 	flag.Parse()
@@ -46,10 +47,9 @@ func main() {
 
 	filepath_db = filepath.Clean(filepath_db)
 
-	file_structure := FileStructure{
+	db_structure := DatabaseStructure{
 		RecordCount: 0,
 		Records:     []Record{},
-		Files:       [][]byte{},
 	}
 
 	if _, err := os.Stat(filepath_db); os.IsNotExist(err) {
@@ -58,19 +58,19 @@ func main() {
 
 		file, err := os.Create(filepath_db)
 		if err != nil {
-			log.Fatal("[MAIN] Error creating file: ", err)
+			log.Fatal("[MAIN] Error creating database: ", err)
 		}
 
 		// Write the first byte (0) to the file
 		write_n, err := file.Write([]byte{0})
 		if err != nil {
-			log.Fatal("[MAIN] Error writing to file: ", err)
+			log.Fatal("[MAIN] Error writing to database: ", err)
 		}
 		// move cursor after first byte
-		cursor_position += uint64(write_n)
+		cursor_position += int64(write_n)
 
 		// start the repl
-		repl(file, &file_structure)
+		repl(file, &db_structure)
 		//file.Close()
 
 	} else if err != nil {
@@ -79,50 +79,43 @@ func main() {
 		// file exists
 		fmt.Println("[MAIN] Reading ", filepath_db)
 		// open file
-		file, err := os.Open(filepath_db)
+		file, err := os.OpenFile(filepath_db, os.O_RDWR, os.ModePerm)
 		if err != nil {
-			log.Fatal("[MAIN] Error Opening File: ", err)
+			log.Fatal("[MAIN] Error opening database: ", err)
 		}
 
 		//read first byte
-		if err := binary.Read(file, binary.LittleEndian, &file_structure.RecordCount); err != nil {
-			log.Fatal("[MAIN] Error Reading First Byte: ", err)
+		if err := binary.Read(file, binary.LittleEndian, &db_structure.RecordCount); err != nil {
+			log.Fatal("[MAIN] Error reading first byte: ", err)
 		}
-		move_cursor(&file_structure.RecordCount)
+		move_cursor(&db_structure.RecordCount)
 
 		// Read Records
-		for i := 0; i < int(file_structure.RecordCount); i++ {
+		for i := 0; i < int(db_structure.RecordCount); i++ {
 			var record Record
 
 			// Read filename
-			filenameBytes := make([]byte, 40)
-			if _, err := file.Read(filenameBytes); err != nil {
-				log.Fatal("[MAIN] Error Reading Filename: ", err)
-			}
-			record.FileName = string(filenameBytes)
-
-			// Read order
-			if err := binary.Read(file, binary.LittleEndian, &record.Order); err != nil {
-				log.Fatal("[MAIN] Error Reading Order: ", err)
+			if err := binary.Read(file, binary.LittleEndian, &record.FileName); err != nil {
+				log.Fatal("[MAIN] Error reading FileName: ", err)
 			}
 
 			// Read size
 			if err := binary.Read(file, binary.LittleEndian, &record.Size); err != nil {
-				log.Fatal("[MAIN] Error Reading Size: ", err)
+				log.Fatal("[MAIN] Error reading size: ", err)
 			}
 
-			file_structure.Records = append(file_structure.Records, record)
+			db_structure.Records = append(db_structure.Records, record)
 		}
-		move_cursor(&file_structure.Records)
+		move_cursor(&db_structure.Records)
 
-		fmt.Printf("[MAIN] %s has %d records and cursor position is at %d\n", file.Name(), file_structure.RecordCount, cursor_position)
-		repl(file, &file_structure)
+		fmt.Printf("[MAIN] %s has %d records and cursor position is at %d\n", file.Name(), db_structure.RecordCount, cursor_position)
+		repl(file, &db_structure)
 		//file.Close()
 	}
 
 }
 
-func repl(file *os.File, fs *FileStructure) {
+func repl(file *os.File, db *DatabaseStructure) {
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("[REPL] Starting Repl")
 ReadLoop:
@@ -130,17 +123,30 @@ ReadLoop:
 		fmt.Print("> ")
 		scanner.Scan()
 		command := scanner.Text()
-
-		switch {
-		case command == "read":
+		if strings.HasPrefix(command, "read") {
 			//readFile()
 			fmt.Println("todo read ")
-		case command == "write":
-			//writeFile()
+		} else if strings.HasPrefix(command, "write") {
+			args := strings.Split(command, " ")
+			var order uint8 = db.RecordCount
+
+			if len(args) == 3 {
+				t_ord, err := strconv.Atoi(args[2])
+				if err != nil {
+					fmt.Println("write <filename> <order|optional>")
+					continue ReadLoop
+				}
+				order = uint8(t_ord)
+			} else if len(args) != 2 {
+				fmt.Println("write <filename> <order|optional>")
+				continue ReadLoop
+			}
+
+			write(file, db, args[1], order)
 			fmt.Println("todo write")
-		case command == "close":
+		} else if strings.HasPrefix(command, "close") {
 			break ReadLoop
-		default:
+		} else {
 			fmt.Println("Unknown command. Please enter read <file>, write <file>, close")
 		}
 	}
@@ -153,5 +159,173 @@ func move_cursor(data any) {
 	if size == -1 {
 		return
 	}
-	cursor_position += uint64(size)
+	cursor_position += int64(size)
+}
+
+// not concurrent for now
+func write(file *os.File, db *DatabaseStructure, filepath string, order uint8) {
+	if order > db.RecordCount {
+		fmt.Println("[WRITE] Order is unusable")
+		return
+	}
+
+	// open file
+	new_file, err := os.Open(filepath)
+	if err != nil {
+		fmt.Println("[WRITE] Error opening source file: ", err)
+		return
+	}
+	defer new_file.Close()
+
+	// Get file size
+	fileInfo, err := new_file.Stat()
+	if err != nil {
+		fmt.Println("[Write] Can't read file ", err)
+		return
+	}
+	file_size := fileInfo.Size()
+	file_name := fileInfo.Name()
+	if record_contains(db, file_name) {
+		fmt.Println("[Write] File already exists ", file_name)
+		return
+	}
+	// Create Record
+	var record Record
+	record.FileName = truncateString(file_name)
+	record.Size = file_size
+
+	fmt.Printf("[WRITE] Writing %s at %d with size %d\n", record.FileName, order, record.Size)
+
+	// Create a temporary file for writing
+	tempFile, err := os.CreateTemp("./", "tempfile")
+	if err != nil {
+		log.Fatal("[WRITE] Temporary file failed to create ", err)
+	}
+
+	metadata_point := binary_size(&db.RecordCount) + int64(binary.Size(Record{})*int(order))
+
+	// Read data from the original file up to the record insertion point and write it to the temporary file
+	_, err = file.Seek(0, io.SeekStart)
+	if err != nil {
+		log.Fatal("[WRITE] Failed to seek start ", err)
+	}
+
+	_, err = io.CopyN(tempFile, file, metadata_point)
+	if err != nil {
+		log.Fatal("[WRITE] Failed to write the old metadata ", err)
+	}
+
+	//fmt.Println("[WRITE] metadata_point: ", metadata_point)
+
+	// Write the new record
+	if err := binary.Write(tempFile, binary.LittleEndian, record.FileName); err != nil {
+		log.Fatal("[WRITE] Failed to write new record name ", err)
+	}
+	if err := binary.Write(tempFile, binary.LittleEndian, record.Size); err != nil {
+		log.Fatal("[WRITE] Failed to write new record size ", err)
+	}
+
+	// get rest
+	left_record_point := (binary_size(&db.RecordCount) + int64(binary.Size(Record{})*int(db.RecordCount))) - metadata_point
+
+	_, err = io.CopyN(tempFile, file, left_record_point)
+	if err != nil {
+		log.Fatal("[WRITE] Failed to write the rest of metadata: ", err)
+	}
+
+	//fmt.Println("[WRITE] left_record_point: ", left_record_point)
+
+	// insertion point
+	var insertion_point int64 = 0
+	for i := 0; i < int(order); i++ {
+		insertion_point += db.Records[i].Size
+	}
+
+	fmt.Println("[WRITE] insertion point: ", insertion_point)
+
+	// Read data from the original file up to the file insertion point and write it to the temporary file
+	_, err = io.CopyN(tempFile, file, insertion_point)
+	if err != nil {
+		log.Fatal("[WRITE] Failed to write the files before: ", err)
+	}
+
+	// Write new file
+	_, err = io.Copy(tempFile, new_file)
+	if err != nil {
+		log.Fatal("[WRITE] Failed to write the new file ", err)
+	}
+
+	// Read the remaining data from the original file and write it to the temporary file
+	_, err = io.Copy(tempFile, file)
+	if err != nil {
+		log.Fatal("[WRITE] Failed to write rest of the files ", err)
+	}
+
+	_, err = tempFile.Seek(0, io.SeekStart)
+	if err != nil {
+		log.Fatal("[WRITE] Error going back to start in temp file ", err)
+	}
+
+	_, err = file.Seek(0, io.SeekStart)
+	if err != nil {
+		log.Fatal("[WRITE] Error going back to start in main file ", err)
+	}
+
+	_, err = io.Copy(file, tempFile)
+	if err != nil {
+		log.Fatal("[WRITE] Failed to write back to database ", err)
+	}
+
+	// get cursor pos
+	n_seek, err := file.Seek(0, io.SeekCurrent)
+	if err != nil {
+		log.Fatal("[WRITE] Error getting cursor position ", err)
+	}
+	cursor_position = int64(n_seek)
+
+	// Write new record in memory
+	db.RecordCount += 1
+	db.Records = append(db.Records, record)
+
+	// Remove (delete) the temporary file
+	tempFile.Close()
+	err = os.Remove(tempFile.Name())
+	if err != nil {
+		log.Fatal("Error removing temporary file:", err)
+	}
+
+}
+
+func binary_size(data any) int64 {
+	size := binary.Size(data)
+	if size == -1 {
+		return 0
+	}
+	return int64(size)
+}
+
+func truncateString(s string) [40]byte {
+
+	// Convert the string to a byte slice
+	stringBytes := []byte(s)
+
+	// Create a fixed-size byte array of length 40
+	var fixedSizeByteArray [40]byte
+
+	// Copy the content of the byte slice into the fixed-size byte array
+	copy(fixedSizeByteArray[:], stringBytes)
+
+	return fixedSizeByteArray
+}
+
+func record_contains(db *DatabaseStructure, filename string) bool {
+	var res bool = false
+
+	for _, v := range db.Records {
+		fnr := string(bytes.TrimRight(v.FileName[:], "\x00"))
+		if fnr == filename {
+			res = true
+		}
+	}
+	return res
 }
